561f25369e9c58be392917563792883283e2e075
luojun <2782180957@qq.com>
2023-08-18 11:20:49 +0800

    Feat: getMsg

--- demo/src/App.java
+++ demo/src/App.java
@@ -8,6 +8,10 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(getMsg());
+    }
+
+    public static String getMsg(){
+        return "Hello World!";
     }
 }
