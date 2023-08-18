e0fae37e96ebdd10acb00ec507d928ce4948ad82
luojun <2782180957@qq.com>
2023-08-18 11:18:10 +0800

    init

---
+++ demo/src/App.java
@@ -0,0 +1,13 @@
+/**
+ * Main
+ *
+ * @author luojun
+ * @version v1.0
+ * @since 2023-08-18
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
